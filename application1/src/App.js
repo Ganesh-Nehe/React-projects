0655346dfb066ef6de5cca56a282281d0071dfc6
ganesh <ganeshnehe07@gmail.com>
2023-10-06 11:58:33 +0530

    update

--- application1/src/App.js
+++ application1/src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Ganesh-Nehe/React-projects/tree/main/application1"
           target="_blank"
           rel="noopener noreferrer"
         >
