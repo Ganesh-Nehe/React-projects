113c8ab67c7fe1ca08b199634b30c78d29568e2a
ganesh <ganeshnehe07@gmail.com>
2023-10-06 11:51:05 +0530

    start

---
+++ application1/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          THIS IS MY FIRST REACT APPLICATION.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          MY GITHUB REPO FOR THIS PROJECT🔗
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
